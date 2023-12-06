014cb35b479c1d34e03a9cb188789f36f120abfe
Debora <sylvianeyerideborada@gmail.com>
2023-12-06 16:03:04 +0000

    create bar search

---
+++ pwa-project/src/components/ContactPicker.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+     <p> Rechercher</p>
+     <input type="text" v-model="searchTerm" placeholder="Rechercher un etudiant"/>
+     <ul>
+      <li v-for="student in filteredStudents" :key="student.id">
+        {{ student.name }}
+      </li>
+    </ul>
+     <button @click="search">Rechercher</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactPicker',
+  props: {
+
+  },
+  data () {
+    return {
+      searchTerm: ''
+    }
+  },
+  methods: {
+    search () {
+      this.$emit('search', this.searchTerm)
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
